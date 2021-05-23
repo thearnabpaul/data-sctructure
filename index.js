97931b428f22ffc155149f7b16184252cf9e6d2a
thearnabpaul <>
2021-05-23 18:18:01 +0000

    new

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
-const boxes = [1,2,3,4,5];
-const items = ['a','b','c','d','e']
-function permu(mask){
-  for (i=0;i<mask.length;i++){
-    for (j=0;j<mask.length;j++){
-      console.log(mask[i],mask[j])
-    }
-  }
-};
 
-function permu1(mask){
-  for (i=0;i<mask.length;i++){
-    for (j=0;j<mask.length;j++){
-      console.log(mask[i],'+',mask[j],'=',mask[i]+mask[j])
-    }
+for (i=0;i>0;i++){
+  for (j=0;j>0;j++){
+    console.log(Math.pow(i,j));
   }
-};
-
-permu(boxes);
-permu1(boxes);
+}
